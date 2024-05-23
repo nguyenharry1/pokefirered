aa5bbb770d5854e5685e13a4b334fe26856b0874
nguyenharry1 <aamgamer9889@gmail.com>
2024-05-23 08:43:21 -0400

    Implemented ipatix's High Quality Audio Mixer. Sample rate is still at the default value due to memory being misaligined when another sample rate was chosen. Ran into an issue where "link_rfu_2" files and related code can't be REMOVED entirely, causing the rom file not being created.

--- src/link_rfu_2.c
+++ src/link_rfu_2.c
@@ -71,12 +71,13 @@ struct RfuDebug
 static EWRAM_DATA INIT_PARAM sRfuReqConfig = {};
 static EWRAM_DATA struct RfuDebug sRfuDebug = {};
 
-static u32 sRfuAPIBuffer[RFU_API_BUFF_SIZE_RAM / 4];
+EWRAM_DATA u32 sRfuAPIBuffer[RFU_API_BUFF_SIZE_RAM / 4];
+EWRAM_DATA struct RfuManager gRfu = {};
+
 static u8 sResendBlock8[CMD_LENGTH * 2];
 static u16 sResendBlock16[CMD_LENGTH];
 
 struct RfuGameData gHostRfuGameData;
-struct RfuManager gRfu;
 u8 gHostRfuUsername[PLAYER_NAME_LENGTH + 1];
 
 static void InitChildRecvBuffers(void);
